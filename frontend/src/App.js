4a37ee505dc4083fd187228b876e1a1b5c567f02
unknown <chasdoughtry@gmail.com>
2025-03-27 18:59:58 -0500

    Main & Favorites pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import Header from './components/header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Favorites from './pages/favorites';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Favorites" element={<Favorites/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+
+
+function Home() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const placeHolder = 'https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U';
+
+  // sample movies
+  const movies = [
+    { id: 1, title: 'Inception', image: placeHolder },
+    { id: 2, title: 'Interstellar', image: placeHolder },
+    { id: 3, title: 'The Dark Knight', image: placeHolder },
+    { id: 4, title: 'The Matrix', image: placeHolder },
+  ];
+
+  // Search Filter 
+  const filteredMovies = movies.filter(movie =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className='container'>
+      <div className='main-content'>
+        <h1>Movies</h1>
+        <div className='searchBar'>
+            <label>Search</label>
+            <input type='text' value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Search movies..."
+            />
+        </div>
+        <div className='moviesContainer'>
+        {filteredMovies.map(movie => (
+            <div key={movie.id} className="movieTile">
+              <img src={movie.image} alt={movie.title} />
+              <h3>{movie.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
