06d79dae2334ef575f9886c79fec3d95d3097983
gabec <gabechoi03@gmail.com>
2025-03-27 22:40:35 -0500

    changing dataset

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Define models
-class Item(Base):
-    __tablename__ = "items"
+class Movie(Base):
+    __tablename__ = "movies"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
