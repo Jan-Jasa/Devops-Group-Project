104d18a85b6beedcd3720770d89507bbbff2e092
Jan-Jasa <janjasa03@gmail.com>
2025-03-27 23:02:09 -0500

    Revert "Removed unnecessary files and fixed frontend homepage" yeah i messeed up
    This reverts commit b3b6aa2429486893f9b4c575d18f9a04d7df55b6.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from "./pages/home";
+import MovieSearch from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <MovieSearch />
   </React.StrictMode>
 );
 
